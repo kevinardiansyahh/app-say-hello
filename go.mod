19911d60ec397fbe874020b2a7c41ba111480d1f
Kevin Ardiansyah H <kevin.hidayat@tokopedia.com>
2022-08-14 19:41:42 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kevinardiansyahh/app-say-hello
+
+go 1.17
+
+require github.com/kevinardiansyahh/go-say-hello v1.5.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	say_hello "github.com/kevinardiansyahh/go-say-hello"
+)
+
+func main() {
+	fmt.Println(say_hello.SayHello())
+}
